c555b2d7df6299138476e6cd06708377a568ab05
Theodore Havelka <tedhavelka66@gmail.com>
2026-08-13 17:58:25 +0000

    app: amend main.c with thread priorities, sleep times and a third thread to
    exercise the first scenario described in lecture 1, task 1.

--- app/src/main.c
+++ app/src/main.c
@@ -5,30 +5,43 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
+#define PRIO_A 7
 #define PRIO_B 5
+#define PRIO_C 3
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        k_msleep(200);
-    }
+	while (1) {
+		LOG_INF("thread a");
+		k_msleep(100);
+	}
 }
 
-void thread_b_fn(void *p1, void *p2, void *p3)
+void t_med_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        k_msleep(300);
-    }
+	while (1) {
+		LOG_INF("thread  b");
+		k_msleep(500);
+	}
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+	while (1) {
+		LOG_INF("thread   c");
+		k_msleep(500);
+	}
+}
+
+K_THREAD_DEFINE(thread_a, STACK_SIZE, t_low_fn,
                 NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
+K_THREAD_DEFINE(thread_b, STACK_SIZE, t_med_fn,
                 NULL, NULL, NULL, PRIO_B, 0, 0);
+K_THREAD_DEFINE(thread_c, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, PRIO_C, 0, 0);
 
 int main(void)
 {
-    return 0;
+	LOG_INF("main.c thread test starting");
+	return 0;
 }
-
